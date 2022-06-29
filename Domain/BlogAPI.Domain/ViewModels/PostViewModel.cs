e3a5021b5e7f11c8fb68f40fdd3528acfd02b539
H0tab <ksasha18@mail.ru>
2022-06-29 22:43:31 +0300

    Added PostBiewModel

---
+++ Domain/BlogAPI.Domain/ViewModels/PostViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogAPI.Domain.ViewModels
+{
+    public class PostViewModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
